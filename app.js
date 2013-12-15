0f6ae4946b14c21f3936fb5a0aaf75fb0ef34ef1
Corey Floyd <coreyfloyd@gmail.com>
2013-12-15 04:16:01 -0500

    test scrape

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var nodeio = require("node.io");
 var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+var stories = [];
+
+nodeio.scrape(function() {
+    this.getHtml('http://www.reddit.com/', function(err, $) {
+        $('a.title').each(function(title) {
+            stories.push(title.text);
+        });
+        this.emit(stories);
+    });
+});
+
+
+app.get('/', function(req, res) {
+  res.send('Hello World! - here are reddit stories: ' stories);
 });
