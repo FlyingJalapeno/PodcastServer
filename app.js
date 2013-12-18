7d8a07a0319ba5f9df0a2991bcc70c006c9e5223
Corey Floyd <coreyfloyd@gmail.com>
2013-12-18 09:18:03 -0500

    scraping with cheerio

--- app.js
+++ app.js
@@ -1,30 +1,91 @@
-var express = require("express");
+//Logging
 var logfmt = require("logfmt");
-var nodeio = require("node.io");
+
+//Web API
+var express = require("express");
+
+//HTTP Connections
+var http = require("http");
+var https = require('https');
+
+//DOM
+var cheerio = require("cheerio");
+
+//Setup server
 var app = express();
-
 app.use(logfmt.requestLogger());
-
 var port = process.env.PORT || 5000;
 
+//Start Server
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
-var stories = [];
-
-nodeio.scrape(function() {
-    this.getHtml('http://www.reddit.com/', function(err, $) {
-
-        $('a.title').each(function(title) {
-			console.log(title.text);
-            stories.push(title.text);
-        });
-		
-		console.log("collected " + stories.length + " stories");
+//Download HTML
+var download = function(url, callback) {
+  https.get(url, function(res) {
+    var data = "";
+    res.on('data', function (chunk) {
+      data += chunk;
     });
+    res.on("end", function() {
+      callback(data);
+    });
+  }).on("error", function() {
+    callback(null);
+  });
+}
+
+
+var scrapePodcasts = function(callback) {
+    
+    var url = "https://itunes.apple.com/us/genre/podcasts/id26?mt=2"
+
+        download(url, function(data) {
+          if (data) {
+     
+            var $ = cheerio.load(data);
+
+            console.log();   
+
+            $('#main', 'body').
+            $('body').children().each(function(index, element) {
+                
+                console.log($(element).attr('class'));   
+             
+            });
+            
+            console.log("----------------------------------");
+    //        console.log("grid: " + $("grid3-column"));
+
+            
+    //	    $("ul > grid-3-column","genre-nav").each(function(index, element) {
+    //	        
+    //            $(element).children().each(function(index, element) {
+    //            
+    //                var genreUrl = $(element).attr("a");
+    //                console.log("scraped genre url: " + genreURL);
+    //                attributes.push(genreUrl);
+    //            
+    //            });
+    //	      
+    //            console.log("genre nav: " + element);
+    //
+    //        });
+
+
+          }
+          else console.log("error");  
+        });
+   
+}
+
+//API GET
+app.get('/', function(req, res) {
+	
+//	res.send("collected genre urls: " + attributes.join(', '));
+				
 });
 
-app.get('/', function(req, res) {
-  res.send('Hello World! - here are reddit stories: ' + stories);
-});
+
+scrapePodcasts();
